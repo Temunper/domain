2a23bb9c9c9f9c2c720626e7a0d8753afd935535
Junpeng <2833551728@qq.com>
2019-05-22 17:59:46 +0800

    删除多余文件

--- domain/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
- //数据库配置
-$config['db'] = ['host'=> 'localhost',
-                 'username'=> 'root',
-                 'password'=>'root',
-                 'db_name' => 'domain_db'];
-$config['defaultController'] = 'Login';
-$config['defaultAction'] = 'login';
-return $config;
